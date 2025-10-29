b4a417d707033942c4308d8a425abfd8d5620c83
TahaKabiri <newemail@example.com>
2025-10-29 20:37:52 +0330

    creat router

---
+++ routes/user.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const {body , validationResult} = require('express-validator');
+
+let users = require('../user-list');
+
+
+
+
+
+router.get('/' , (req,res)=>{
+    
+    res.json({data:users});
+})
+router.get('/:id' , (req,res)=>{
+    let user = users.find((user)=>{
+        if(user.id == req.params.id){
+       return user ;
+        };
+    });
+
+    res.json({
+        data:user, 
+    });
+
+});
+
+router.post('/' , [
+    body('email' , 'email is empty').isEmail(),
+    body('password' , 'password min 5 length').isLength({ min : 5}) 
+] ,  (req,res)=>{
+
+const error = validationResult(req);
+if(!error.isEmpty()){
+    return res.status(422).json({
+        data:null ,
+        message : error.array()
+    });
+}
+
+    console.log(req.body);
+    req.body.id = parseInt(req.body.id);
+    users.push(req.body);
+    res.json({
+        data:users,
+        message:'کاربر اد شد '
+    });
+});
+
+router.put('/:id' , (req,res)=>{
+  users = users.map(user=>{
+    if(user.id = req.params.id){
+        return req.body ;
+    }else{
+        return user;
+    }
+  })
+  res.json({
+    data: users ,
+    message : ' succssiful'
+  })
+})
+router.delete('/:id' , (req,res)=>{
+    users = users.filter(user=>{
+        if(user.id != req.params.id){
+            return user;
+        }
+    })
+
+    res.json({
+        data : 'user delet'
+    })
+});
+
+
+module.exports = router ; 
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express') ;
 const app = express();
-const {body , validationResult} = require('express-validator');
+
 
 
 global.config = require('./config/config.js');
-let users = require('./user-list.js');
+
 
 
 app.use(express.static(__dirname + "/public"));
@@ -12,69 +12,7 @@ app.use (express.urlencoded({extended : false}));
 app.use(express.json());
 
 
-app.get('/' , (req,res)=>{
-    
-    res.json({data:users});
-})
-app.get('/:id' , (req,res)=>{
-    let user = users.find((user)=>{
-        if(user.id == req.params.id){
-       return user ;
-        };
-    });
-
-    res.json({
-        data:user, 
-    });
-
-})
-
-app.post('/' , [
-    body('email' , 'email is empty').isEmail(),
-    body('password' , 'password min 5 length').isLength({ min : 5}) 
-] ,  (req,res)=>{
-
-const error = validationResult(req);
-if(!error.isEmpty()){
-    return res.status(422).json({
-        data:null ,
-        message : error.array()
-    });
-}
-
-    console.log(req.body);
-    req.body.id = parseInt(req.body.id);
-    users.push(req.body);
-    res.json({
-        data:users,
-        message:'کاربر اد شد '
-    });
-});
-
-app.put('/:id' , (req,res)=>{
-  users = users.map(user=>{
-    if(user.id = req.params.id){
-        return req.body ;
-    }else{
-        return user;
-    }
-  })
-  res.json({
-    data: users ,
-    message : ' succssiful'
-  })
-})
-app.delete('/:id' , (req,res)=>{
-    users = users.filter(user=>{
-        if(user.id != req.params.id){
-            return user;
-        }
-    })
-
-    res.json({
-        data : 'user delet'
-    })
-})
+app.use('/api/user' , require('./routes/user.js'));
 
  
 app.listen(config.port , ()=> {
