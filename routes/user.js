055f53c9a720ce68f55bad9f27eb898daedab515
TahaKabiri <newemail@example.com>
2025-11-05 14:28:48 +0330

    connect project & router => mongoose

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const router = express.Router();
+let Users = require('./../model/user');
 const {body , validationResult} = require('express-validator');
 
 
-let users = require('../user-list');
 
 
-
-
-
-router.get('/' , (req,res)=>{
-    
+router.get('/' , async (req,res)=>{
+    let users = await Users.find({});
     res.render('user' , {users : users , title : 'همه کاربران ' ,
         errors : req.flash('errors') , message : req.flash('message')});
 })
-router.get('/:id' , (req,res)=>{
-    let user = users.find((user)=>{
-        if(user.id == req.params.id){
-       return user ;
-        };
-    });
-
+router.get('/:id' , async(req,res)=>{
+    let user = await Users.findById(req.params.id)
+    
     res.render('update-user' , { user : user})
 });
 
@@ -28,8 +21,8 @@ router.post('/' , [
     body('email' , 'Enter your email.').isEmail(),
     body('password' , 'The minimum password characters are 5 ').isLength({ min : 5}) ,
     body('firstname' , 'Write your name, Martike.').isLength({ min : 2}),
-    body('id' , 'Complete the ID').isLength({ min : 1})
-] ,  (req,res)=>{
+   
+] ,  async (req,res)=>{
 
 const error = validationResult(req);
 if(!error.isEmpty()){
@@ -39,30 +32,25 @@ if(!error.isEmpty()){
 
     console.log(req.body);
     req.body.id = parseInt(req.body.id);
-    users.push(req.body);
+    let newUser = await new Users({
+        firstname : req.body.firstname ,
+        email : req.body.email ,
+        password : req.body.password 
+    })
+    await newUser.save();
     req.flash('message' , 'User created successfully.');
     res.redirect('/api/user')
 });
 
-router.put('/:id' , (req,res)=>{
-  users = users.map(user=>{
-    if(user.id = req.params.id){
-        return req.body ;
-    }else{
-        return user;
-    }
-  })
+router.put('/:id' , async (req,res)=>{
+  const users = await Users.updateOne({_id : req.params.id} , {$set : req.body});
   req.flash('message' , 'user update is Successflly');
   res.redirect('/api/user');
 
   })
 
-router.delete('/:id' , (req,res)=>{
-    users = users.filter(user=>{
-        if(user.id != req.params.id){
-            return user;
-        }
-    })
+router.delete('/:id' , async (req,res)=>{
+  await Users.deleteOne({_id : req.params.id});
 
     req.flash('message' , ' user deleted is Successflly')
     res.redirect('/api/user');
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const methodOverride = require('method-override');
 const cookieparser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
-
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 global.config = require('./config/config.js');
-
+ mongoose.connect('mongodb://127.0.0.1:27017/nodestart')
+ .then(() => console.log('Connected!'))
+ .catch((e)=>console.error(e));
 
 
 app.use(express.static(__dirname + "/public"));
@@ -17,9 +19,9 @@ app.use(express.json());
 app.set('view engine' , 'ejs');
 app.use(methodOverride('method'));
 
-app.use(cookieparser('jfkajgfjkjnhjfnhgjrabgfbdahfbrhg'));
+app.use(cookieparser(process.env.cookiparser_key));
 app.use(session({
-    secret:'asdfasdf65f6ffdsfdwe',
+    secret:process.env.session_key,
     resave:true,
     saveUninitialized : true ,
 }));
